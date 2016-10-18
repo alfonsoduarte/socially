ea44c8d135943d6e23d2fa50e4e14446a9392d47
unknown <alfonsoduarte@gmail.com>
2016-10-18 08:03:25 -0600

    Dos

---
+++ imports/ui/components/partiesList/partiesList.js
@@ -0,0 +1,32 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import template from './partiesList.html';
+import { Parties } from '../../../api/parties';
+import { name as PartyAdd } from '../partyAdd/partyAdd';
+
+class PartiesList {
+  constructor($scope, $reactive) {
+    'ngInject';
+ 
+    $reactive(this).attach($scope);
+ 
+    this.helpers({
+      parties() {
+        return Parties.find({});
+      }
+    });
+  }
+}
+ 
+const name = 'partiesList';
+ 
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  PartyAdd
+]).component(name, {
+  template,
+  controllerAs: name,
+  controller: PartiesList
+});
